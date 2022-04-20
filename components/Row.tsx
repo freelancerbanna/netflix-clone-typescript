5ede811a5e8c557081f5adfb03e4c0acff30be10
freelancerbanna <hasanulhaquebanna@gmail.com>
2022-04-20 16:09:50 +0600

    scroll effect added

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,5 +1,8 @@
+import React, { useRef, useState } from 'react'
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
-import React from 'react'
+import classNames from 'classnames'
+
+//
 import { Movie } from '../typings'
 import Thumbnail from './Thumbnail'
 
@@ -9,6 +12,26 @@ interface Props {
 }
 
 const Row = ({ title, movies }: Props) => {
+  const ref = useRef<HTMLDivElement>(null)
+  const [isMoved, setIsMoved] = useState(false)
+
+  const handleClick = (direction: string) => {
+    setIsMoved(true)
+    if (ref.current) {
+      const { scrollLeft, clientWidth } = ref.current
+
+      console.log({ scrolLef: scrollLeft })
+      console.log({ clientWidth: clientWidth })
+
+      const scrollToWay =
+        direction === 'left'
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth
+
+      ref.current.scrollTo({ left: scrollToWay, behavior: 'smooth' })
+    }
+  }
+
   return (
     <div className="h-48 space-y-0.5 md:space-y-3">
       <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
@@ -16,12 +39,17 @@ const Row = ({ title, movies }: Props) => {
       </h2>
       <div className="group relative md:-ml-2">
         <ChevronLeftIcon
-          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 `}
+          className={classNames(
+            'absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100',
+            !isMoved && 'hidden'
+          )}
+          onClick={() => handleClick('left')}
         />
 
         {/*  */}
         <div
-          className="scrollbar-hide flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2"
+          ref={ref}
+          className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
           //   ref={rowRef}
         >
           {movies.map((movie) => (
@@ -29,7 +57,10 @@ const Row = ({ title, movies }: Props) => {
           ))}
         </div>
 
-        <ChevronRightIcon className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100" />
+        <ChevronRightIcon
+          className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100"
+          onClick={() => handleClick('right')}
+        />
       </div>
     </div>
   )
